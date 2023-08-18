4631a9a3968097b740de4875b0ffe1cbdc30fb35
KhushbooVarathe <khushboo.va@cisinlabs.com>
2023-08-18 14:36:36 +0530

    add file

---
+++ src/components/First.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div>MY_FIRST_PAGE</div>
+  )
+}
+
+export default First
---
+++ src/components/Second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Second() {
+  return (
+    <div>MY_Second_PAGE</div>
+  )
+}
+
+export default Second
